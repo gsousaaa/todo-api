1403e811c0db59170f06f5fc1db5b3f6d5ab0738
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-10 11:03:08 -0300

    Config inicial

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server rodando na porta ${PORT}`)
+})
+
+
