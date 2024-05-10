29a44af40fba1e7df676ff6c6d4d30805ec06903
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-10 13:56:01 -0300

    Declarando e configurando o model User

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes  } from 'sequelize';
+import { sequelize } from '../instances/mysql';
+
+interface UserInstance extends Model {
+    id: number,
+    username: string,
+    email: string,
+    passwordHash: string
+}
+
+export const User = sequelize.define<UserInstance>('User', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    }, 
+    username: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    email: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    passwordHash: {
+        allowNull: false,
+        type: DataTypes.STRING
+    }
+}, {
+    tableName: 'users',
+    timestamps: false
+})
+
+
