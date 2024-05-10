8808aec3a32b4e7bed8fa51d7790c560b5e6439e
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-10 14:41:03 -0300

    Declarando e configurando o model Task.

---
+++ src/models/Task.ts
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../instances/mysql';
+import { getTokenSourceMapRange } from 'typescript';
+
+interface TaskInstance extends Model {
+    taskId: number,
+    title: string,
+    description: string,
+    isCompleted?: boolean,
+    list_id: number
+}
+
+
+export const Task = sequelize.define<TaskInstance>('Task', {
+        taskId: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        title: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        description: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        isCompleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        listId: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        }
+})
