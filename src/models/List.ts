e514b9e8b653f2216e22532d24dc0b9eb6c17093
gsousaaa <glaucoezequiel94@gmail.com>
2024-05-10 13:56:14 -0300

    Declarando e configurando o model List

---
+++ src/models/List.ts
@@ -0,0 +1,25 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../instances/mysql';
+
+interface ListInstance extends Model {
+    listId: number,
+    title: string
+    user_id: number
+}
+
+export const List = sequelize.define<ListInstance>('List', {
+        listId: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER
+        },
+        title: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        user_id: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        }
+})
+
